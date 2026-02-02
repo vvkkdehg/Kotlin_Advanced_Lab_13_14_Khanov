f3b6faa821cece8d7c01af0d5b7936d112fe6468
Vlados <hanovvladik@mail.ru>
2026-02-02 22:11:38 +0300

    Rewrite extensions using interface

--- src/Question.kt
+++ src/Question.kt
@@ -9,7 +9,22 @@ enum class Difficulty{
     HARD
 }
 
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) {print("▓")}
+        repeat(Quiz.total - Quiz.answered) {print("▒")}
+        println()
+        println(progressText)
+    }
+
     val question1 = Question<String>(
         questionText = "Речка спятила с ума — По домам пошла сама. ___",
         answer = "водопровод",
@@ -33,19 +48,8 @@ class Quiz {
         var answered: Int = 3
     }
 }
-
-val Quiz.StudentProgress.progressText: String
-    get() = "${answered} of ${total} answered"
-
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.answered) {print("▓")}
-    repeat(Quiz.total - Quiz.answered) {print("▒")}
-    println()
-    println(Quiz.progressText)
-}
-
 fun main() {
-    Quiz.printProgressBar()
+    Quiz().printProgressBar()
 }
 
 
