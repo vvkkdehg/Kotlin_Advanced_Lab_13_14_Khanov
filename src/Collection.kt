81775a5e8eb558d41513373e9b01b482d9d51b6d
Vlados <hanovvladik@mail.ru>
2026-02-02 22:54:51 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -18,16 +18,42 @@ val newSolarSystem = arrayOf(
 
 
 fun main() {
-    println(solarSystem[0])
-    println(solarSystem[1])
-    println(solarSystem[2])
-    println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
-    println(newSolarSystem[8])
+//    println(solarSystem[0])
+//    println(solarSystem[1])
+//    println(solarSystem[2])
+//    println(solarSystem[3])
+//    println(solarSystem[4])
+//    println(solarSystem[5])
+//    println(solarSystem[6])
+//    println(solarSystem[7])
+//    solarSystem[3] = "Little Earth"
+//    println(solarSystem[3])
+//    println(newSolarSystem[8])
 //    solarSystem[8] = "Pluto"
+    val solarSystem = mutableListOf(
+        "Mercury", "Venus", "Earth", "Mars",
+        "Jupiter", "Saturn", "Uranus", "Neptune"
+    )
+
+//    println(solarSystem.size)
+//
+//    println(solarSystem[2])
+//    println(solarSystem.get(3))
+//
+//    println(solarSystem.indexOf("Earth"))
+//    println(solarSystem.indexOf("Pluto"))
+
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+
+    solarSystem[3] = "Future Moon"
+    println(solarSystem[3])
+    println(solarSystem[9])
+
+    solarSystem.removeAt(9)
+    println(solarSystem.contains(("Pluto")))
+    println("Future Moon" in solarSystem)
 }
