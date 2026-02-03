a15198cf7be1a7f33de1fa4d2f38161f23e7397e
Vlados <hanovvladik@mail.ru>
2026-02-03 15:18:10 +0300

    map() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -49,7 +49,15 @@ val cookies = listOf(
     )
 )
 fun main() {
-    cookies.forEach {
-        println("Пункт меню: ${it.name}")
+//    cookies.forEach {
+//        println("Пункт меню: ${it.name}")
+//    }
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
+    }
+
+    println("Полное меню:")
+    fullMenu.forEach {
+        println(it)
     }
 }
