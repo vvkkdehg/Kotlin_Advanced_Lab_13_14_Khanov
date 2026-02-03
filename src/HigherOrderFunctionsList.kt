8e1043d8f9942b3884954029d689f6ccffb60585
Vlados <hanovvladik@mail.ru>
2026-02-03 15:24:14 +0300

    groupBy() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -60,12 +60,25 @@ fun main() {
 //    fullMenu.forEach {
 //        println(it)
 //    }
-    val softBakedMenu = cookies.filter {
-        it.softBaked
+//    val softBakedMenu = cookies.filter {
+//        it.softBaked
+//    }
+//
+//    println("Мягкое печенье:")
+//    softBakedMenu.forEach {
+//        println("${it.name} - ${it.price}$")
+//    }
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu = groupedMenu[true] ?: emptyList()
+    val crunchyMenu = groupedMenu[false] ?: emptyList()
+
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - ${it.price}$")
     }
 
-    println("Мягкое печенье:")
-    softBakedMenu.forEach {
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
         println("${it.name} - ${it.price}$")
     }
 }
